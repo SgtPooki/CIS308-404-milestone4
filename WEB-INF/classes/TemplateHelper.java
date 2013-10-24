17969f2d03946d6a351aa6b4815fdcc9bbb99464
SgtPooki <sgtpooki@gmail.com>
2013-10-24 01:49:17 -0500

    giving up on implementing templateHelper the professional way

--- WEB-INF/classes/TemplateHelper.java
+++ WEB-INF/classes/TemplateHelper.java
@@ -5,12 +5,23 @@
 //TODO: Make this function pull this HTML from a file in a folder called templates.. so we can easily change our html!! i.e. templates/sidebar.html. 
 package data;
 
+import java.io.*;
+import javax.servlet.ServletContext;
+
 public class TemplateHelper {
 
-  public TemplateHelper() {
+
+  public TemplateHelper(){
   }
 
-  public String getSideBarHTML() {
-	return "WEB-INF/templates/sidebar.html";
+  public String getSideBarHTML() throws FileNotFoundException, IOException{
+
+    return "<div class=\"sidebar\">" + 
+    "<p><a href=\"index.jsp\" class=\"button\">HOME</a></p><br>" + 
+    "<p><a href=\"DisplayData.jsp\" class=\"button\">Display Data</a></p><br>" + 
+    "<p><a href=\"FormPost.jsp\" class=\"button\">Place Order</a></p><br>" + 
+    "<p><a href=\"CreateTable.jsp\" class=\"button\">Create Database</a></p><br>" + 
+    "<p><a href=\"NukeDatabase.jsp\" class=\"button\">Delete Data</a></p><br>" + 
+    "</div>";
   }
 }
